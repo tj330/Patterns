f4ecad85574a23fed64416d1bb33e89e07a908a7
tj330 <142474733+tj330@users.noreply.github.com>
2023-12-12 23:28:57 +0530

    Update pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -1,4 +1,4 @@
-/#prints a pyramid first by printing spaces and then * accordingly
+#prints a pyramid first by printing spaces and then * accordingly
 i=5
 for j in range(i):
     print(" "*(i-j), "*"*(2*j+1))
